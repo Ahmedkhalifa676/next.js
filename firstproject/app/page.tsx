1ccee9fa97defa7c938e1f0f44ac9926271a9e2a
Ahmed Reda Khalifa <ak4717202@gmail.com>
2026-07-19 14:33:12 +0300

    make routings

--- firstproject/app/page.tsx
+++ firstproject/app/page.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      
+    <div className='bg-white w-full h-dvh'>
+      <header className='w-full flex gap-3 items-center justify-center p-3'>
+      <a href=''><button className='btn btn-primary w-[8rem]'>Home</button></a>
+      <a href='/about'><button className='btn btn-primary w-[8rem]'>About</button></a>
+      <a href='/products'><button className='btn btn-primary w-[8rem]'>Products</button></a>
+      <a href='/settings'><button className='btn btn-primary w-[8rem]'>Settings</button></a>
+      <a href='/profile'><button className='btn btn-primary w-[8rem]'>Profile</button></a>
+      </header>
     </div>
   )
 }
